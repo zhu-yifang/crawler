907f146c33083beccce51c0eb614197d1b6d3214
zhu-yifang <zhuyifang@reed.edu>
2021-04-26 01:21:52 -0700

    4/26 final project
    practice with urllib
    learned how to pretend to be a web browser
    learned how to get webpage by urllib

--- crawler.py
+++ crawler.py
@@ -3,8 +3,10 @@
 # @file: crawler.py
 from bs4 import BeautifulSoup	#website, crawl data
 import re
-import urllib
+import urllib.request
 import xlwt
+import ssl
+context = ssl._create_unverified_context()
 
 def main():
 	baseurl = "https://movie.douban.com/top250?start="
@@ -13,12 +15,44 @@ def main():
 	# 2. analyze data
 	savepath = ".\\doubanMovieTop250.xls"
 	# 3. save data
-	saveData(savepath)
-	
+	# saveData(savepath)
+
+	askURL("https://movie.douban.com/top250?start=")
 # Crawl the website
 def getData(baseurl):
 	datalist = []
+	for i in range(0,10):
+		url = baseurl + str(i*25)
+		html = askURL(url)	# save the web source code
+
+		# analyze each webpage
+
+
+
 	return datalist
+
+# get the content of a specific url
+def askURL(url):
+	# pretend to be a web browser
+	head = {	# stimulate to be a head
+		"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) "
+					  "Version/14.0.2 Safari/605.1.15"
+	}
+	# try to request the url
+	request = urllib.request.Request(url = url, headers = head)
+	html = ""
+	try:
+		response = urllib.request.urlopen(request, context=context)
+		html = response.read().decode()
+		print(html)
+	except urllib.error.URLError as e:
+		if hasattr(e,"code"):
+			print(e, code)
+		if hasattr(e, "reason"):
+			print(e.reason)
+
+	return html
+
 # save data
 def saveData(savepath):
 	pass
--- test/testUrllib.py
+++ test/testUrllib.py
@@ -1,5 +1,7 @@
 import urllib.request
+import ssl
 
+context = ssl._create_unverified_context()
 # get
 # response = urllib.request.urlopen("http://www.google.com")
 # print(response.read())
@@ -9,3 +11,35 @@ import urllib.request
 # data = bytes(urllib.parse.urlencode({"hello":"world"}),encoding="utf-8")
 # response = urllib.request.urlopen("http://httpbin.org/post",data=data)
 # print(response.read().decode())
+
+# timeout
+# try:
+#     response = urllib.request.urlopen("http://httpbin.org/get", timeout=1)
+#     print(response.read().decode())
+# except urllib.error.URLError as e:
+#     print("time out!")
+
+# response = urllib.request.urlopen("http://httpbin.org/get", timeout=1)
+# print(response.status)
+# print(response.getheaders())
+
+# url = "https://www.douban.com"
+# url = "http://httpbin.org/post"
+# headers={
+# "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) "
+#               "Version/14.0.2 Safari/605.1.15"
+# }
+#
+# data = bytes(urllib.parse.urlencode({"name":"eric"}), encoding = "utf-8")
+# req = urllib.request.Request(url = url, data = data, headers = headers, method = "POST")
+# response = urllib.request.urlopen(req)
+# print(response.read().decode('utf-8'))
+
+url = "https://www.reed.edu"
+headers={
+"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) "
+              "Version/14.0.2 Safari/605.1.15"
+}
+req = urllib.request.Request(url = url, headers = headers)
+response = urllib.request.urlopen(req, context = context)
+print(response.read().decode('utf-8'))
